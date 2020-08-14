5dafd182d63a971038f5c524fa093a33fe2e0f0d
phlipTilahun <philiptilahun@gmail.com>
2020-08-14 10:41:43 +0300

    db

---
+++ dbscript.sql
@@ -0,0 +1,47 @@
+create table ticket (
+
+)
+
+create table ticketBuyer (
+
+)
+
+create table eventOrganizer(
+
+)
+
+create table event (
+
+)
+
+create table hall (
+
+)
+
+create table venue (
+
+)
+
+create table seatingChart (
+
+)
+
+create table seat (
+   seatnum integer
+)
+
+create table eventReport (
+
+)
+
+create table history (
+
+)
+
+create table validator (
+
+)
+
+create table admin (
+   
+)
